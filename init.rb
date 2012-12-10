f543a42badc2525f35203fc6b1afa112cc0f8240
Joao Costa <jcosta@imaginarycloud.com>
2012-12-10 11:30:23 +0000

    changed module name

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ Redmine::Plugin.register :redmine_evm do
   description 'Graphical indicators for EV Management'
   version '0.9.0'
   url 'http://imaginarycloud.com'
-  author_url 'mailto:jcosta@imaginarycloud.com'
+  author_url 'mailto:info@imaginarycloud.com'
 
-  project_module :ratios do
+  project_module :evm do
 	 permission :ratios, { :ratios => [:index] }, :public => true
   end
   menu :project_menu, :ratios, { :controller => 'ratios', :action => 'index' }, :caption => 'EVM', :after => :files
