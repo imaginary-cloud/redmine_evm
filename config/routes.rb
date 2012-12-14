a22285cd58a17e07fc33c1e47f6ac31ed3f3ebf5
tfranco <tfranco@imaginarycloud.com>
2012-12-14 11:51:45 +0000

    Removes app name from route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-RedmineApp::Application.routes.draw do
-  match 'projects/:id/ratios/:action', :to => 'ratios#index', :via => [:get]
-end
+match 'projects/:id/ratios/:action', :to => 'ratios#index', :via => [:get]
