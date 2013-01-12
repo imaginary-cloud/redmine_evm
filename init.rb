1fc33c4642eb649ac0f132045b09bf7ad7bbf47b
tfranco <tfranco@imaginarycloud.com>
2013-01-12 21:38:06 +0000

    Packages version 0.9.1.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_evm do
   name 'Redmine EVM plugin'
   author 'Imaginary Cloud (http://imaginarycloud.com)'
   description 'Graphical indicators for Earned Value Management'
-  version '0.9.0'
+  version '0.9.1'
   url 'https://github.com/imaginary-cloud/redmine_evm'
   author_url 'mailto:info@imaginarycloud.com'
 
