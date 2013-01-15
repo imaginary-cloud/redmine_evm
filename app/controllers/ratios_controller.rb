2b5f7a6ee01a4e5dbb03f717b2233955e6344530
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-01-15 15:14:28 +0900

    use two spaces instead of tabs for indents

--- app/controllers/ratios_controller.rb
+++ app/controllers/ratios_controller.rb
@@ -1,44 +1,44 @@
 class RatiosController < ApplicationController
-	unloadable
-	include IndicatorsLogic
+  unloadable
+  include IndicatorsLogic
 
-	helper :ratios
-	include RatiosHelper
+  helper :ratios
+  include RatiosHelper
 
-	def index
-		@project = Project.find(params[:id])
-		respond_to do |format|
-			format.html do
-				@evms = []
-				data = IndicatorsLogic::retrive_data(@project)
-				@evms << evm(@project)
-				@project.versions.where(:status=>"open").each do |my_version|
-					data = IndicatorsLogic::retrive_data(my_version)
-					evm_hash = evm(my_version)
-					evm_hash[:version] = my_version
-					@evms << evm_hash
-				end
-				render :action => 'index'
-			end
-			format.csv do
-				if params[:version].present?
-					ver = @project.versions.find(params[:version])
-					evm_hash = evm(ver)
-					csv_fn = "evm-project-#{@project.id}-version-#{ver.id}.csv"
-				else
-					evm_hash = evm(@project)
-					csv_fn = "evm-project-#{@project.id}.csv"
-				end
-				send_data(evm_csv(evm_hash), :type => 'text/csv; header=present', :filename => csv_fn)
-			end
-		end
-	end
+  def index
+    @project = Project.find(params[:id])
+    respond_to do |format|
+      format.html do
+        @evms = []
+        data = IndicatorsLogic::retrive_data(@project)
+        @evms << evm(@project)
+        @project.versions.where(:status=>"open").each do |my_version|
+          data = IndicatorsLogic::retrive_data(my_version)
+          evm_hash = evm(my_version)
+          evm_hash[:version] = my_version
+          @evms << evm_hash
+        end
+        render :action => 'index'
+      end
+      format.csv do
+        if params[:version].present?
+          ver = @project.versions.find(params[:version])
+          evm_hash = evm(ver)
+          csv_fn = "evm-project-#{@project.id}-version-#{ver.id}.csv"
+        else
+          evm_hash = evm(@project)
+          csv_fn = "evm-project-#{@project.id}.csv"
+        end
+        send_data(evm_csv(evm_hash), :type => 'text/csv; header=present', :filename => csv_fn)
+      end
+    end
+  end
 
 private
 
-	def evm(proj_or_ver)
-		data = IndicatorsLogic::retrive_data(proj_or_ver)
-		{ :name => proj_or_ver.name,
-			:indicators => IndicatorsLogic::calc_indicators(proj_or_ver, data[0], data[1]) }
-	end
+  def evm(proj_or_ver)
+    data = IndicatorsLogic::retrive_data(proj_or_ver)
+    { :name => proj_or_ver.name,
+      :indicators => IndicatorsLogic::calc_indicators(proj_or_ver, data[0], data[1]) }
+  end
 end
--- app/helpers/ratios_helper.rb
+++ app/helpers/ratios_helper.rb
@@ -1,24 +1,24 @@
 module RatiosHelper
-	def evm_csv(evm)
-		decimal_separator = l(:general_csv_decimal_separator)
-		export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
-			fields = ["", "cpi", "spi"]
-			csv << fields.collect {|c| Redmine::CodesetUtil.from_utf8(
-																			c,
-																			l(:general_csv_encoding))}
-			fields = []
-			fields << evm[:name]
-			fields << evm[:indicators][1].to_s.gsub('.', decimal_separator)
-			fields << evm[:indicators][2].to_s.gsub('.', decimal_separator)
-			csv << fields.collect {|c| Redmine::CodesetUtil.from_utf8(
-																		 c,
-																		 l(:general_csv_encoding))}
-			evm[:indicators][0].each do |ent|
-				csv << ent.collect {|c| Redmine::CodesetUtil.from_utf8(
-																			 c.to_s.gsub('.', decimal_separator),
-																			 l(:general_csv_encoding))}
-				end
-		end
-		export
-	end
+  def evm_csv(evm)
+    decimal_separator = l(:general_csv_decimal_separator)
+    export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
+      fields = ["", "cpi", "spi"]
+      csv << fields.collect {|c| Redmine::CodesetUtil.from_utf8(
+                                      c,
+                                      l(:general_csv_encoding))}
+      fields = []
+      fields << evm[:name]
+      fields << evm[:indicators][1].to_s.gsub('.', decimal_separator)
+      fields << evm[:indicators][2].to_s.gsub('.', decimal_separator)
+      csv << fields.collect {|c| Redmine::CodesetUtil.from_utf8(
+                                     c,
+                                     l(:general_csv_encoding))}
+      evm[:indicators][0].each do |ent|
+        csv << ent.collect {|c| Redmine::CodesetUtil.from_utf8(
+                                       c.to_s.gsub('.', decimal_separator),
+                                       l(:general_csv_encoding))}
+        end
+    end
+    export
+  end
 end
