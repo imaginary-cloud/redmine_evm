2cf13e4c31873580ec9de407229101a89629d251
tfranco <tfranco@imaginarycloud.com>
2013-01-17 00:35:23 +0000

    Renames some vars on indicators logic.

--- lib/indicators_logic.rb
+++ lib/indicators_logic.rb
@@ -2,46 +2,46 @@ module IndicatorsLogic
 
   def self.retrive_data(project_or_version)
     if project_or_version.instance_of? Version
-      my_project = project_or_version.project
-      my_version = project_or_version
-      ary_reported_time_week_year =
-        my_project.time_entries.where(
-          :issue_id => Issue.where(:fixed_version_id => my_version.id)).sum(
+      project = project_or_version.project
+      version = project_or_version
+      time_entries_by_week_and_year =
+        project.time_entries.where(
+          :issue_id => Issue.where(:fixed_version_id => version.id)).sum(
             :hours, :group => [:tweek, :tyear])
-      ary_all_issues = my_version.fixed_issues
+      issues = version.fixed_issues
     else
-      my_project = project_or_version
-      ary_reported_time_week_year =
-        my_project.time_entries.sum(
+      project = project_or_version
+      time_entries_by_week_and_year =
+        project.time_entries.sum(
           :hours, :group => [:tweek, :tyear])
-      ary_all_issues = my_project.issues
+      issues = project.issues
     end
-    [ary_reported_time_week_year, ary_all_issues]
+    [time_entries_by_week_and_year, issues]
   end
 
-  def self.calc_indicators(my_project_or_version, ary_reported_time_week_year, ary_all_issues)
-    check_end_date = my_project_or_version.due_date || Time.now.to_date
+  def self.calc_indicators(project_or_version, time_entries_by_week_and_year, issues)
+    end_date = project_or_version.due_date || Time.now.to_date
     check_ary_reported_time_week_year =
-      ary_reported_time_week_year.empty? ? Time.now.to_date :
-        Date.ordinal(ary_reported_time_week_year.keys.last[1],
-                       ary_reported_time_week_year.keys.last[0] * 7 - 3)
-    if ary_all_issues.maximum(:start_date)
-      check_ary_all_issues = ary_all_issues.empty? ? Time.now.to_date : ary_all_issues.maximum(:start_date)
+      time_entries_by_week_and_year.empty? ? Time.now.to_date :
+        Date.ordinal(time_entries_by_week_and_year.keys.last[1],
+                       time_entries_by_week_and_year.keys.last[0] * 7 - 3)
+    if issues.maximum(:start_date)
+      check_ary_all_issues = issues.empty? ? Time.now.to_date : issues.maximum(:start_date)
       my_project_or_version_end_date =
-          [check_end_date, check_ary_reported_time_week_year, check_ary_all_issues].max
+          [end_date, check_ary_reported_time_week_year, check_ary_all_issues].max
     else
       my_project_or_version_end_date =
-          [check_end_date, check_ary_reported_time_week_year].max
+          [end_date, check_ary_reported_time_week_year].max
     end
     ary_weeks_years = []
     real_start_date = [
-          (my_project_or_version.start_date.nil? ?
+          (project_or_version.start_date.nil? ?
             (Time.now.to_date - 1.day) :
-              my_project_or_version.start_date.beginning_of_week),
-          (ary_reported_time_week_year.empty? ?
+              project_or_version.start_date.beginning_of_week),
+          (time_entries_by_week_and_year.empty? ?
             Time.now.to_date :
-            Date.ordinal(ary_reported_time_week_year.keys.first[1],
-                           ary_reported_time_week_year.keys.first[0] * 7 - 3))
+            Date.ordinal(time_entries_by_week_and_year.keys.first[1],
+                           time_entries_by_week_and_year.keys.first[0] * 7 - 3))
         ].min
     while real_start_date < my_project_or_version_end_date + 1.week
       ary_weeks_years << [real_start_date.cweek, real_start_date.cwyear]
@@ -50,9 +50,9 @@ module IndicatorsLogic
     hash_weeks_years = {}
     ary_weeks_years.each{|e| hash_weeks_years[e] = [0, 0, 0]}
     done_ratio = 0
-    ary_all_issues.each do |issue|
+    issues.each do |issue|
       next if !issue.leaf?
-      start_issue_date = issue.start_date? ? issue.start_date : my_project_or_version.start_date
+      start_issue_date = issue.start_date? ? issue.start_date : project_or_version.start_date
       end_issue_date = issue.due_date? ? issue.due_date : my_project_or_version_end_date
       estimated_time = issue.estimated_hours? ? issue.estimated_hours : 0
       done_ratio = (issue.done_ratio / 100.0)
@@ -76,7 +76,7 @@ module IndicatorsLogic
     sum_earned = 0
     ary_weeks_years.each do |k|
       v = hash_weeks_years[k]
-      sum_real += ary_reported_time_week_year.has_key?(k)? ary_reported_time_week_year[k] : 0
+      sum_real += time_entries_by_week_and_year.has_key?(k)? time_entries_by_week_and_year[k] : 0
       v[0] = sum_real
       sum_planned += v[1]
       v[1] = sum_planned
