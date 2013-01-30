ca0daa2a962e1c16fda52fb2d5376853870b4884
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-01-30 15:53:36 +0900

    remove issue description from unit test

--- test/unit/lib/indicators_logic_test.rb
+++ test/unit/lib/indicators_logic_test.rb
@@ -13,7 +13,7 @@ class IndicatorsLogicTest < ActiveSupport::TestCase
     issue1 = Issue.new(:project_id => project.id, :tracker_id => 1, :author_id => 3,
                        :status_id => 1, :priority => IssuePriority.all.first,
                        :subject => 'issue1',
-                       :description => 'IssueTest#test_create')
+                       :description => '')
     assert issue1.save
     issue1.estimated_hours = 3
     issue1.start_date = '2013-01-24'
