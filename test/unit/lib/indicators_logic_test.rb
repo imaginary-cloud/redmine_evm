6415d24c6d2e87b689d2edc2f3c30460d4e34b6b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-01-30 17:17:33 +0900

    use :non_working_week_days setting in unit test

--- test/unit/lib/indicators_logic_test.rb
+++ test/unit/lib/indicators_logic_test.rb
@@ -8,23 +8,25 @@ class IndicatorsLogicTest < ActiveSupport::TestCase
   include IndicatorsLogic
 
   def test_one_issue
-    project = Project.new(:name => 'evm', :identifier => 'evm', :tracker_ids => [1])
-    assert project.save
-    issue1 = Issue.new(:project_id => project.id, :tracker_id => 1, :author_id => 3,
-                       :status_id => 1, :priority => IssuePriority.all.first,
-                       :subject => 'issue1',
-                       :description => '')
-    assert issue1.save
-    issue1.estimated_hours = 3
-    issue1.start_date = '2013-01-24'
-    issue1.due_date   = '2013-01-24'
-    issue1.done_ratio = 50
-    assert issue1.save
-    assert_equal 3, issue1.estimated_hours
-    assert_equal '2013-01-24'.to_date, project.start_date
+    with_settings :non_working_week_days => %w(6 7) do
+      project = Project.new(:name => 'evm', :identifier => 'evm', :tracker_ids => [1])
+      assert project.save
+      issue1 = Issue.new(:project_id => project.id, :tracker_id => 1, :author_id => 3,
+                         :status_id => 1, :priority => IssuePriority.all.first,
+                         :subject => 'issue1',
+                         :description => '')
+      assert issue1.save
+      issue1.estimated_hours = 3
+      issue1.start_date = '2013-01-24'
+      issue1.due_date   = '2013-01-24'
+      issue1.done_ratio = 50
+      assert issue1.save
+      assert_equal 3, issue1.estimated_hours
+      assert_equal '2013-01-24'.to_date, project.start_date
 
-    indicators = IndicatorsLogic::calc_indicators(project)
-    arr = indicators[0]
-    assert_equal ["4/2013", 0.0, 3.0, 1.5], arr[1]
+      indicators = IndicatorsLogic::calc_indicators(project)
+      arr = indicators[0]
+      assert_equal ["4/2013", 0.0, 3.0, 1.5], arr[1]
+    end
   end
 end
