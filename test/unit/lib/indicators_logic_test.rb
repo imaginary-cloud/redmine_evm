3a7d5aec43ea8a75692f9f4582ff820562179da5
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-01-30 17:18:40 +0900

    add unit test with time entry

--- test/unit/lib/indicators_logic_test.rb
+++ test/unit/lib/indicators_logic_test.rb
@@ -29,4 +29,36 @@ class IndicatorsLogicTest < ActiveSupport::TestCase
       assert_equal ["4/2013", 0.0, 3.0, 1.5], arr[1]
     end
   end
+
+  def test_one_issue_one_time_entry
+    with_settings :non_working_week_days => %w(6 7) do
+      project = Project.new(:name => 'evm', :identifier => 'evm', :tracker_ids => [1])
+      assert project.save
+      issue1 = Issue.new(:project_id => project.id, :tracker_id => 1, :author_id => 3,
+                         :status_id => 1, :priority => IssuePriority.all.first,
+                         :subject => 'issue1',
+                         :description => '')
+      assert issue1.save
+      issue1.estimated_hours = 3
+      issue1.start_date = '2013-01-24'
+      issue1.due_date   = '2013-01-24'
+      issue1.done_ratio = 50
+      assert issue1.save
+      assert_equal 3, issue1.estimated_hours
+      assert_equal '2013-01-24'.to_date, project.start_date
+
+      anon     = User.anonymous
+      activity = TimeEntryActivity.find_by_name('Design')
+      TimeEntry.create(:spent_on => '2013-01-24',
+                       :hours    => 1,
+                       :issue    => issue1,
+                       :project  => project,
+                       :user     => anon,
+                       :activity => activity)
+
+      indicators = IndicatorsLogic::calc_indicators(project)
+      arr = indicators[0]
+      assert_equal ["4/2013", 1.0, 3.0, 1.5], arr[1]
+    end
+  end
 end
