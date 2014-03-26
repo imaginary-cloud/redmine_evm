c402e683c14776ebbca194747dc070d6402f694d
Nuno <ncastro@imaginarycloud.com>
2014-03-26 11:30:30 +0000

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_evm do
+  name 'Redmine EVM plugin'
+  author 'Imaginary Cloud (http://imaginarycloud.com)'
+  description 'Graphical indicators for Earned Value Management'
+  version '0.0.1'
+  url 'https://github.com/imaginary-cloud/redmine_evm'
+  author_url 'mailto:info@imaginarycloud.com'
+end
