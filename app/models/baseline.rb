fbd048e973f8bce93820f44f5b3f36eeccb68884
Nuno <ncastro@imaginarycloud.com>
2014-03-27 17:01:15 +0000

    Fix the error of the baseline relation

--- app/models/baseline.rb
+++ app/models/baseline.rb
@@ -2,7 +2,7 @@ class Baseline < ActiveRecord::Base
   unloadable
 
   belongs_to :project
-  has_and_belongs_to_many :issues
-  has_and_belongs_to_many :versions
+  has_and_belongs_to_many :baseline_issues
+  has_and_belongs_to_many :baseline_versions
 
 end
