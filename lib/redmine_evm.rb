29f18e30503a7d70d482a0e8d5da606490f74c7c
João Rodrigues <jonezine@msn.com>
2014-03-28 11:41:14 +0000

    Add UI tab Baselines under Project Settings

--- lib/redmine_evm.rb
+++ lib/redmine_evm.rb
@@ -1,3 +1,4 @@
 Rails.configuration.to_prepare do
   require 'redmine_evm/patches/project_patch'
+  require 'redmine_evm/patches/projects_helper_patch'
 end
---
+++ lib/redmine_evm/patches/projects_helper_patch.rb
@@ -0,0 +1,32 @@
+module RedmineEvm
+  module Patches
+    module ProjectsHelperPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+
+        base.class_eval do
+          unloadable
+
+          alias_method_chain :project_settings_tabs, :baselines
+        end
+      end
+
+      module InstanceMethods
+        def project_settings_tabs_with_baselines
+          tabs = project_settings_tabs_without_baselines
+
+          tabs.push({ :name => 'baselines',
+                      :action => :baselines,
+                      :partial => 'projects/baselines_settings',
+                      :label => :label_baseline_plural})
+
+          tabs
+        end
+      end
+    end
+  end
+end
+
+unless ProjectsHelper.included_modules.include?(RedmineEvm::Patches::ProjectsHelperPatch)
+  ProjectsHelper.send(:include, RedmineEvm::Patches::ProjectsHelperPatch)
+end
