0665a65701cc9b3ea128c959c972a6632750d7fe
Nuno <ncastro@imaginarycloud.com>
2014-04-14 10:25:55 +0100

    Deletes active model and add the project actual cost module path to rails configuration

--- lib/redmine_evm.rb
+++ lib/redmine_evm.rb
@@ -1,4 +1,5 @@
 Rails.configuration.to_prepare do
   require 'redmine_evm/patches/project_patch'
   require 'redmine_evm/patches/projects_helper_patch'
+  require 'redmine_evm/patches/project_actual_cost_patch'
 end
