ed537ddb189209c22d1392e0fc63dd96ae788ae2
João Rodrigues <jonezine@msn.com>
2014-04-14 10:27:10 +0100

    Fix EVM controller (refs #8031)

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -1,9 +1,12 @@
 class EvmsController < ApplicationController
   unloadable
 
+  respond_to :html, :json
+
   def index
     @project = Project.find(params[:id])
-    @baselines = @project.baselines
+    @baselines = @project.baselines.all
+    respond_with(@baselines)
   end
 
 end
