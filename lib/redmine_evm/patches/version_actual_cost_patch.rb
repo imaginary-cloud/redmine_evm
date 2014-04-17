6480067208b4fee40239e34209c2e451802645bc
Nuno <ncastro@imaginarycloud.com>
2014-04-17 18:33:35 +0100

    Add actual cost and earned value methods to versions (refs #8509)

---
+++ lib/redmine_evm/patches/version_actual_cost_patch.rb
@@ -0,0 +1,77 @@
+module RedmineEvm
+  module Patches
+    module VersionActualCostPatch
+      def self.included(base) # :nodoc:
+
+        base.extend(ClassMethods)
+
+        base.send(:include, VersionInstanceMethods)
+
+        base.class_eval do
+          unloadable # Send unloadable so it will not be unloaded in development
+        end
+      end
+    end
+    module ClassMethods
+
+      def VersionActualCostPatch.included(mod)
+        puts "#{self} esta a ser incluido em #{mod}"
+      end
+
+    end
+
+    module VersionInstanceMethods
+
+      def get_start_date
+        start_date || created_on
+      end
+
+      def end_date
+        date = created_on.to_date
+        fixed_issues.each do |issue|
+          date = issue.time_entries.maximum('spent_on') if(issue.time_entries.maximum('spent_on')>date)
+        end
+        date
+      end
+
+      def actual_cost
+        puts self.class
+        actual_cost = 0;
+        fixed_issues.each do |issue|
+          unless issue.time_entries.nil?
+            actual_cost += issue.time_entries.sum(:hours)  
+          end
+        end
+        actual_cost
+      end
+
+      def get_time_entries
+        time_entries = []
+        fixed_issues.each do |issue|
+          unless issue.time_entries.nil?
+            issue.time_entries.each do|entry|
+              time_entries << entry  
+            end
+          end
+        end 
+        time_entries
+      end
+
+      def actual_cost_by_week
+        actual_cost_by_weeks = {}
+        time = 0
+        time_entries = get_time_entries
+        (get_start_date.to_date..end_date.to_date).each do |key| 
+          time += time_entries.select{ |date| date.spent_on == key }.sum(&:hours)
+          actual_cost_by_weeks[key.beginning_of_week] = time
+        end
+        actual_cost_by_weeks
+      end
+
+    end
+  end
+end
+
+unless Version.included_modules.include?(RedmineEvm::Patches::VersionActualCostPatch)
+  Version.send(:include, RedmineEvm::Patches::VersionActualCostPatch)
+end
