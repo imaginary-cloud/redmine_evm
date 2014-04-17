5e71b55df2f6d1ba3e271d59bda1cd5aa196fae9
João Rodrigues <jonezine@msn.com>
2014-04-17 18:46:25 +0100

    Move chart_data action to evms_controller and refactor some code (refs #8031)

--- app/controllers/evms_controller.rb
+++ app/controllers/evms_controller.rb
@@ -1,12 +1,50 @@
 class EvmsController < ApplicationController
   unloadable
 
+  include EvmsHelper
+
   respond_to :html, :json, :js
 
   def index
     @project = Project.find(params[:project_id])
     @baselines = @project.baselines.order( 'created_on DESC' ) #Order to get the current baseline first.
     respond_with(@baselines)
+  end
+
+  def chart_data
+    project = Project.find(params[:project_id])
+    baseline = Baseline.find(params[:id])
+
+    data_to_chart = {}
+    data_to_chart['pv'] = convert_to_chart(baseline.planned_value_by_week)
+    data_to_chart['ac'] = convert_to_chart(project.actual_cost_by_week)
+    data_to_chart['ev'] = convert_to_chart(project.earned_value_by_week)
+
+    respond_to do |format|
+      format.json { render :json => data_to_chart }
+    end
+  end
+
+  def version_chart_data
+    project = Project.find(params[:project_id])
+    baseline = Baseline.find(params[:id])
+
+    # baseline_versions = baseline.baseline_versions
+    # baseline_versions.each do |bv|
+    #   bv.planned_value_by_week
+    #   version = Version.find(bv.original_version_id)
+    #   version.actual_cost_by_week
+    #   version.earned_value_by_week
+    # end
+
+    data_to_chart = {}
+    data_to_chart['pv'] = convert_to_chart(baseline.planned_value_by_week)
+    data_to_chart['ac'] = convert_to_chart(project.actual_cost_by_week)
+    data_to_chart['ev'] = convert_to_chart(project.earned_value_by_week)
+
+    respond_to do |format|
+      format.json { render :json => data_to_chart }
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-
+resources :baselines
 resources :projects do 
 	resources :baselines
-	get '/baselines/:id/chart_data' => 'baselines#chart_data', as: :baseline_chart_data
-end
-
-resources :baselines
-
-resources :projects do
 	resources :evms
+	get '/baselines/:id/chart_data' => 'evms#chart_data', as: :baseline_chart_data
 end
