9df1aa6ba69e6fce9969ca7579c3b51bd8edd8aa
Nuno <ncastro@imaginarycloud.com>
2014-04-17 11:01:58 +0100

    Update planned value by week method to make issues always return a due date value (refs #8024)

--- app/models/schedulable.rb
+++ app/models/schedulable.rb
@@ -7,31 +7,11 @@ module Schedulable
 
   def planned_value_by_week
     planned_value_by_weeks = {}
-    #sorted_issues = baseline_issues.sort_by {|date| date.end_date}
     time = 0
-    (start_date.to_date...end_date.to_date).each do |key|
-      time += baseline_issues.where(due_date: key).sum(:estimated_time)
+    (start_date.to_date..end_date.to_date).each do |key|
+      time += baseline_issues.select{ |i| i.end_date == key }.sum(&:estimated_time)
       planned_value_by_weeks[key.beginning_of_week] = time
     end
-    time += baseline_issues.where(due_date: nil).sum(:estimated_time) 
-    planned_value_by_weeks[end_date.to_date.beginning_of_week] = time
     planned_value_by_weeks
-
-    # planned_value_by_weeks = {}
-    # (start_date.to_date...end_date.to_date).each do |key|
-    #   planned_value_by_weeks[key.beginning_of_week] = 0
-    # end
-
-    # sorted_issues = baseline_issues.sort_by {|date| date.end_date}
-    # time = 0
-
-    # sorted_issues.each do |issue|
-    #   issues_week =  issue.end_date.beginning_of_week 
-    #   unless issues_week.nil? || issue.estimated_time.nil?
-    #     time += issue.estimated_time
-    #     planned_value_by_weeks[issues_week] = time
-    #   end
-    # end
-    # planned_value_by_weeks
   end
 end
