b74e855f0e7a0109714b7d2baa15c11a06bdf7c6
João Rodrigues <jonezine@msn.com>
2014-04-27 23:38:22 +0100

    Change evm_controller_test to match role permissions (refs #8583)

--- test/functional/evms_controller_test.rb
+++ test/functional/evms_controller_test.rb
@@ -7,8 +7,17 @@ class EvmsControllerTest < ActionController::TestCase
   ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/../fixtures/',
     [:baselines])
 
+  def setup
+    RedmineEvm::TestCase.prepare
+
+    @controller = EvmsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    User.current = nil
+  end
+
   def test_index
-  	@request.session[:user_id] = 2
+  	@request.session[:user_id] = 1
   	get :index, :project_id => '1'
   	assert_response :success
   end
