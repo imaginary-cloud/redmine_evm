9fd95066bf712afbf2bf552d13a1f762b3de92e1
Nuno <ncastro@imaginarycloud.com>
2014-04-30 12:18:19 +0100

    Removes irrevelent code from projet end date method (refs #8498)

--- lib/redmine_evm/patches/project_actual_cost_patch.rb
+++ lib/redmine_evm/patches/project_actual_cost_patch.rb
@@ -27,7 +27,7 @@ module RedmineEvm
       end
 
       def end_date
-        due_date || self.baselines.find_by_state("Current").due_date
+        self.baselines.find_by_state("Current").due_date
       end
 
       def actual_cost
@@ -37,8 +37,9 @@ module RedmineEvm
       def actual_cost_by_week
         actual_cost_by_weeks = {}
         time = 0
+   
         unless time_entries.maximum('spent_on').nil?
-          (get_start_date.to_date..time_entries.maximum('spent_on').to_date).each do |key| 
+          (get_start_date.to_date..end_date.to_date).each do |key| 
             time += time_entries.where(spent_on: key).sum(:hours)
             actual_cost_by_weeks[key.beginning_of_week] = time
           end
