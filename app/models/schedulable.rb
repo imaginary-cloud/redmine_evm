d60e80da741701bc3f16ed93ffa0963bd9c2969d
Nuno <ncastro@imaginarycloud.com>
2014-04-30 12:06:58 +0100

    Changes variable name in planned value by week to make the code more easy to read

--- app/models/schedulable.rb
+++ app/models/schedulable.rb
@@ -9,7 +9,7 @@ module Schedulable
     planned_value_by_weeks = {}
     time = 0
     (start_date.to_date..end_date.to_date).each do |key|
-      time += baseline_issues.select{ |i| i.end_date == key }.sum(&:estimated_time)
+      time += baseline_issues.select{ |baseline_issue| baseline_issue.end_date == key }.sum(&:estimated_time)
       planned_value_by_weeks[key.beginning_of_week] = time
     end
     planned_value_by_weeks
