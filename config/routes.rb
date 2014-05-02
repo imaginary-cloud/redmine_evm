9cb759cbf0c55ba780d6a9a583f26cd03ba16f0c
João Rodrigues <jonezine@msn.com>
2014-05-02 17:15:42 +0100

    Update routes .

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@
 resources :baselines
 resources :projects do 
 	resources :baselines
-	resources :evms
-	get '/baselines/:id/chart_data' => 'evms#chart_data', as: :chart_data
-	get '/baselines/:id/versions_chart_data' => 'evms#versions_chart_data', as: :versions_chart_data
-	get '/baselines/:id/evm_variables' => 'evms#evm_variables', as: :evm_variables
+	resources :evms, only: :index
 end
