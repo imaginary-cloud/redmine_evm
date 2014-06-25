20a3ce8222a7b4cd3145cc103c5749aeac0e6c4a
Nuno <ncastro@imaginarycloud.com>
2014-06-25 11:02:53 +0100

    Add a maximum date method to make the end date suggestion for baseline be more precise

--- lib/redmine_evm/patches/project_patch.rb
+++ lib/redmine_evm/patches/project_patch.rb
@@ -1,13 +1,35 @@
 module RedmineEvm
   module Patches
+
     module ProjectPatch
       def self.included(base) # :nodoc:
+
+        base.extend(ClassMethods)
+
+        base.send(:include, ProjectInstanceMethods)
+
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
           has_many :baselines
         end
       end
     end
+
+    module ClassMethods  
+    end
+
+    module ProjectInstanceMethods
+      def maximum_date
+        maximum_start_date ||= [
+        issues.maximum('start_date'),
+        shared_versions.maximum('effective_date'),
+        Issue.fixed_version(shared_versions).maximum('start_date')
+        ].compact.max
+        
+        [maximum_start_date,due_date].max
+      end
+    end
+
   end
 end
 
