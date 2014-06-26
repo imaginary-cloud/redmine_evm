de4e90cb7782b5c00e2e5ab538fda1c5bed7dc26
Nuno <ncastro@imaginarycloud.com>
2014-06-26 19:03:06 +0100

    Add the baseline version logic to his own model

--- app/models/baseline_version.rb
+++ app/models/baseline_version.rb
@@ -5,12 +5,12 @@ class BaselineVersion < ActiveRecord::Base
   belongs_to :baseline 
   has_many :baseline_issues, dependent: :destroy
 
-  def end_date
-    @end_date = effective_date || baseline.due_date
+  def start_date # start date for chart?
+    @start_date ||= baseline_issues.minimum('start_date') #se nao houver start date explode!
   end
 
-  def is_excluded
-    self.exclude
+  def end_date # end date for chart?
+    @end_date = effective_date || baseline.due_date
   end
 end
 
