ee643998365f25854c96b67fe32cf4851b0be0ed
Nuno <ncastro@imaginarycloud.com>
2014-06-26 19:02:02 +0100

    Change create issues and create versions method and add the new start date method

--- app/models/baseline.rb
+++ app/models/baseline.rb
@@ -9,6 +9,7 @@ class Baseline < ActiveRecord::Base
 
   validates :name, :due_date, :presence => true
 
+  # scope :closed, lambda { |id| query }
 
   before_create {update_baseline_status("#{l(:label_old_baseline)}", project_id)}
   after_destroy {update_baseline_status("#{l(:label_current_baseline)}", project_id)}
@@ -23,12 +24,8 @@ class Baseline < ActiveRecord::Base
     unless versions.nil?
       versions.each do |version|
         versions_to_exclude.nil? ? exclude = false : exclude =  versions_to_exclude.include?(version.id)
-        if version.closed? && update_estimated_hours == "1"
-        baseline_versions.create(original_version_id: version.id, effective_date: version.get_end_date(self.id), start_date: version.start_date || version.get_start_date(self.id), name: version.name, exclude: exclude)  
-        else
-        baseline_versions.create(original_version_id: version.id, effective_date: version.due_date, start_date: version.start_date || version.get_start_date(self.id), name: version.name, exclude: exclude)
-        #baseline_versions.create(original_version_id: version.id, effective_date: version.get_end_date(self.id), start_date: version.get_start_date(self.id), name: version.name, exclude: exclude)
-        end
+        update_estimated_hours == "1" ? update_hours = true : update_hours = false
+        baseline_versions.create original_version_id: version.id, effective_date: version.get_end_date(self.id), name: version.name, exclude: exclude, update_hours: update_hours
       end
     end
   end
@@ -36,49 +33,23 @@ class Baseline < ActiveRecord::Base
   def create_issues issues, update_estimated_hours
     unless issues.nil?
       issues.each do |issue|
-        baseline_issue = BaselineIssue.new(original_issue_id: issue.id, done_ratio: issue.done_ratio, status: issue.status.name, due_date: issue.due_date, start_date: issue.start_date || issue.created_on, exclude: false)
+        update_estimated_hours == "1" ? update_hours = true : update_hours = false
+        baseline_issue = BaselineIssue.new original_issue_id: issue.id, done_ratio: issue.done_ratio, status: issue.status.name, due_date: issue.due_date, start_date: issue.start_date, exclude: false, update_hours: update_hours, estimated_hours: issue.estimated_hours, spent_hours: issue.spent_hours, closed_on: issue.closed_on
 
         baseline_version = self.baseline_versions.find_by_original_version_id(issue.fixed_version_id)
         unless baseline_version.nil?
           baseline_issue.baseline_version_id = baseline_version.id
           baseline_issue.exclude = baseline_version.exclude
         end
-
-        if update_estimated_hours == "1"
-          puts issue.status.name
-          if issue.status.name == "Closed"
-            baseline_issue.estimated_hours = issue.spent_hours
-            if issue.due_date.nil?
-              issue.time_entries.empty? ? baseline_issue.due_date = issue.updated_on.to_date : baseline_issue.due_date = issue.time_entries.maximum('spent_on')
-            end  
-          else
-            baseline_issue.estimated_hours = issue.estimated_hours || 0
-            #issue.due_date.nil? ? baseline_issue.due_date = issue.time_entries.maximum('spent_on') : baseline_issue.due_date = issue.due_date
-          end
-        else
-          baseline_issue.estimated_hours = issue.estimated_hours || 0
-          if issue.status.name == "Closed"
-            if issue.due_date.nil?
-              issue.time_entries.empty? ? baseline_issue.due_date = issue.updated_on.to_date : baseline_issue.due_date = issue.time_entries.maximum('spent_on')
-            end
-          end
-          # if issue.done_ratio == 100 || issue.status.name == "Rejected"
-          #   issue.time_entries.empty? ? baseline_issue.due_date = issue.updated_on.to_date : baseline_issue.due_date = issue.time_entries.maximum('spent_on')
-          # else
-          #   issue.due_date.nil? ? baseline_issue.due_date = issue.time_entries.maximum('spent_on') : baseline_issue.due_date = issue.due_date
-          # end
-        end 
-
         baseline_issue.save
         baseline_issues << baseline_issue
-        
       end
     end
   end
 
-  def versions_to_exclude operator, selected_target_versions, project_id
+  def versions_to_exclude operator, selected_target_versions
     target_versions = []
-    all_versions = Project.find(project_id).versions.map(&:id)
+    all_versions = project.versions.map(&:id)
     unless selected_target_versions.nil?
       target_versions = selected_target_versions.collect{|v| v.to_i}
     end
@@ -110,6 +81,10 @@ class Baseline < ActiveRecord::Base
       baseline.state = status 
       baseline.save
     end
+  end
+
+  def start_date #delete start date from the database
+    @start_date = baseline_issues.where(exclude: false).minimum(:start_date) || project.created_on #SCOPE WHERE
   end
 
   def end_date
