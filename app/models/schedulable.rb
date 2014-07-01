47731b7aad0853487be550deb6c51a462c7a7d31
Nuno <ncastro@imaginarycloud.com>
2014-07-01 12:40:04 +0100

    Add unless condition to planned value calculation to be do done on nil class

--- app/models/schedulable.rb
+++ app/models/schedulable.rb
@@ -15,7 +15,7 @@ module Schedulable
 
   #Returns the planned value distrubeted by weeks
   def planned_value_by_week
-    @planned_value_by_week ||= calculate_planned_value_by_week
+    @planned_value_by_week ||= calculate_planned_value_by_week 
   end
 
   #Returns the actual (today's date) planned value.
@@ -30,12 +30,13 @@ module Schedulable
       (start_date.beginning_of_week..end_date).each do |date|
         planned_value_by_week[date.beginning_of_week] = 0
       end
-
-      baseline_issues.each do |baseline_issue|
-        next if baseline_issue.exclude || baseline_issue.estimated_hours_for_chart == 0
-        baseline_issue.days.each do |day|
-          planned_value_by_week[day.beginning_of_week] += baseline_issue.hours_per_day unless planned_value_by_week[day.beginning_of_week].nil?
-        end
+      unless baseline_issues.empty?
+        baseline_issues.each do |baseline_issue|
+          next if baseline_issue.exclude || baseline_issue.estimated_hours_for_chart == 0
+          baseline_issue.days.each do |day|
+            planned_value_by_week[day.beginning_of_week] += baseline_issue.hours_per_day unless planned_value_by_week[day.beginning_of_week].nil?
+          end
+        end  
       end
       planned_value_by_week.each_with_object({}) { |(k, v), h| h[k] = v + (h.values.last||0)  }
     end
