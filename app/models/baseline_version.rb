35ba70da7051d09de587593d8e8f5df5feb9c5f6
Nuno <ncastro@imaginarycloud.com>
2014-07-04 16:29:09 +0100

    Refactor end date for baseline version so when update hours it will get the correct date

--- app/models/baseline_version.rb
+++ app/models/baseline_version.rb
@@ -10,8 +10,14 @@ class BaselineVersion < ActiveRecord::Base
     @start_date ||= baseline_issues.minimum('start_date') || created_on.to_date #created_on of the normal original version. rename it in te database
   end
 
-  def end_date # end date for chart?
-    @end_date = effective_date || baseline.due_date
+  def end_date # end date for chart? #com update_hours o caso muda # se não tiver closed issues como é? max due date? metodo privado para max de varias hipoteses?
+    update_hours ? @end_date ||= max_end_date_when_updated_hours.to_date : @end_date ||= effective_date || baseline.due_date
+  end
+
+  private
+
+  def max_end_date_when_updated_hours # repensar nisto
+    baseline_issues.maximum('closed_on') || [effective_date, baseline.due_date].compact.max
   end
 end
 
