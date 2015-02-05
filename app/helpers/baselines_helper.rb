91aa41915cc401161762ef80e15b85a3cf412019
Pedro Rolo <prolo@imaginarycloud.com>
2015-02-05 13:49:49 +0000

    Fixed the text on EVM details when schedule or budget are 0 #54

--- app/helpers/baselines_helper.rb
+++ app/helpers/baselines_helper.rb
@@ -3,6 +3,8 @@ module BaselinesHelper
   def label_cost_performance_percentage
     if @baseline.schedule_variance < 0
       ((1 - @baseline.schedule_performance_index) * 100).round
+    elsif @baseline.schedule_variance.round == 0
+      nil
     else
       ((@baseline.schedule_performance_index - 1) * 100).round
     end
@@ -11,6 +13,8 @@ module BaselinesHelper
   def label_cost_performance_status
     if @baseline.schedule_variance < 0
       l(:label_behind_schedule)
+    elsif @baseline.schedule_variance.round == 0
+      l(:label_on_schedule)
     else
       l(:label_ahead_schedule)
     end
@@ -19,6 +23,8 @@ module BaselinesHelper
   def label_schedule_performance_percentage
     if @baseline.cost_variance < 0
       ((1 - @baseline.cost_performance_index) * 100).round
+    elsif @baseline.cost_variance.round == 0
+      nil
     else
       ((@baseline.cost_performance_index - 1) * 100).round
     end
@@ -27,10 +33,11 @@ module BaselinesHelper
   def label_schedule_performance_status
     if @baseline.cost_variance < 0
       l(:label_above_budget)
+    elsif @baseline.cost_variance.round == 0
+      l(:label_on_budget)
     else
       l(:label_under_budget)
     end
-  end 
-
+  end
 
 end
--- lib/redmine_evm/patches/issue_patch.rb
+++ lib/redmine_evm/patches/issue_patch.rb
@@ -44,36 +44,36 @@ module RedmineEvm
       end
 
       private
-        def dates
-          dates = []
-          selected_journals = journals.select {|journal| journal.journalized.done_ratio > 0}
-          dates[0] = selected_journals.first.created_on unless selected_journals.first.nil?
-          dates[0] = start_date? ? start_date : created_on if dates[0].nil?
+      def dates
+        dates = []
+        selected_journals = journals.select { |journal| journal.journalized.done_ratio > 0 }
+        dates[0] = selected_journals.first.created_on unless selected_journals.first.nil?
+        dates[0] = start_date? ? start_date : created_on if dates[0].nil?
 
-          closed? ? dates[1] = closed_on : dates[1] = updated_on
+        closed? ? dates[1] = closed_on : dates[1] = updated_on
 
-          dates
-        end
+        dates
+      end
 
-        def number_of_days
-          days.size
-        end
+      def number_of_days
+        days.size
+      end
 
-        def estimated_hours_for_chart update_hours, baseline_id
-          baseline_issue =  baseline_issues.find_by_baseline_id(baseline_id)
+      def estimated_hours_for_chart update_hours, baseline_id
+        baseline_issue = baseline_issues.find_by_baseline_id(baseline_id)
 
-          if update_hours && closed? && baseline_issue.is_closed
-            baseline_issue.spent_hours || 0
-          else
-            baseline_issue.estimated_hours || 0
-          end
-
+        if update_hours && closed? && baseline_issue.is_closed
+          baseline_issue.spent_hours || 0
+        else
+          baseline_issue.estimated_hours || 0
         end
 
       end
+
     end
   end
+end
 
-  unless Issue.included_modules.include?(RedmineEvm::Patches::IssuePatch)
-    Issue.send(:include, RedmineEvm::Patches::IssuePatch)
-  end
+unless Issue.included_modules.include?(RedmineEvm::Patches::IssuePatch)
+  Issue.send(:include, RedmineEvm::Patches::IssuePatch)
+end
