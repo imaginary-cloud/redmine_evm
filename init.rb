494d451261d63be529e2a91b6d13500041ba7166
Bruno Sousa <bsousa@imaginaycloud.com>
2018-06-18 14:08:16 +0100

    Version 1.0.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_evm do
   name 'Redmine EVM plugin'
   author 'Imaginary Cloud (http://imaginarycloud.com)'
   description 'Graphical indicators for Earned Value Management'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/imaginary-cloud/redmine_evm'
   author_url 'mailto:info@imaginarycloud.com'
 
